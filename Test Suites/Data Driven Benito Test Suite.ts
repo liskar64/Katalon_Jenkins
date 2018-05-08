6dc795270a8eadbb7b63dc4d0f4dbfd0d4c0d73c
liskar64 <liskar64@gmail.com>
2018-05-08 14:38:33 +0200

    Subida 08/05/2018

--- Test Suites/Data Driven Benito Test Suite.ts
+++ Test Suites/Data Driven Benito Test Suite.ts
@@ -4,7 +4,7 @@
    <name>Data Driven Benito Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-07T08:53:29</lastRun>
+   <lastRun>2018-05-08T12:21:34</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
